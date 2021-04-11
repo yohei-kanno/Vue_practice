827779a2855a61311e016b18962db51b71e031f9
youheikanno <tkro.812.wkrni@icloud.com>
2021-04-12 03:09:02 +0900

    2nd commit

--- APIpracticeInVue/class_bind.js
+++ APIpracticeInVue/class_bind.js
@@ -1,13 +1,41 @@
 var app = new Vue({
   el: '#app',
   data: {
+    message: "",
+    colors: ["blue","green","red"],
     isLarge: true,
     hasError: true,
-    largeClass: 'large',
-    dangerClass: 'text-danger',
+    largeClass: {'large':true},
+    dangerClass:{'text-danger':true},
     classObject:{
       large: true,
       'text-danger': true
+    },
+    largeClass2: {
+      large: true,
+      'bg-gray': true,
+    },
+    dangerClass2: {
+      'text-danger': true
+    },
+    isLarge2: true,
+    color: 'blue',
+    fontSize: '36px',
+    
+    styleObject:{
+      color: `${"green"}`,
+      fontSize: '36px',
+    },
+    
+    toggle: false,
+    count: 1
+  },
+  methods:{
+    clickHandler: function(e){
+      this.count = this.count * e
+    },
+    clickNowTime: function(){
+      this.message = new Date().toLocaleTimeString()
     }
   }
 })
